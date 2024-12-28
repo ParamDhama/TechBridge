29532c9b2958d7c35a8814f1f7e4eabee5b3333c
Param Dhama <paramdhama.mail@gmail.com>
2024-12-28 05:47:22 +0530

    Update apiClient.js

--- Frontend/src/api/apiClient.js
+++ Frontend/src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:3000", // Replace with your backend base URL
+  baseURL: "https://techbridge-qt0l.onrender.com", // Replace with your backend base URL
   headers: {
     "Content-Type": "application/json",
   },
